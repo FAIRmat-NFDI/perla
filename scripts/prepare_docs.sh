17e06b4679bfe58d7d36a4513618d7300b827724
Pepe Marquez <pepemarquezprieto@gmail.com>
2026-01-23 15:09:26 +0100

    Updates

--- scripts/prepare_docs.sh
+++ scripts/prepare_docs.sh
@@ -59,6 +59,7 @@ NOTEBOOKS=(
     "ml-distribution-shift-case-study.ipynb"
     "perovscribe-analysis.ipynb"
     "perovskite-paperbot-plot.ipynb"
+    "physics_filter.ipynb"
 )
 
 # Copy specific notebooks
